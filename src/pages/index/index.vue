fa4295b5742afd1c8bc68ce9879d66c26205c05c
cjh <1964646121@qq.com>
2023-03-12 13:18:34 +0800

    样式调整

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,6 +1,10 @@
 <template>
-  <view class="content">
-    <div v-for="item in list" @click="go(item.url)">{{ item.text }}</div>
+  <view class="tools-list p-16">
+    <div v-for="item in list" @click="go(item.url)" class="tools-item-wrapper">
+      <div class="tools-item flex-center border-radius-4">
+        {{ item.text }}
+      </div>
+    </div>
   </view>
 </template>
 
@@ -10,7 +14,7 @@ export default Vue.extend({
   data() {
     return {
       list: [
-        { text: '图片加水印', url: '/pages/waterMarker/index' },
+        { text: '图片水印', url: '/pages/waterMarker/index' },
         { text: '神秘数字', url: '/pages/mysteriousNumber/index' },
       ],
     }
@@ -24,4 +28,23 @@ export default Vue.extend({
 })
 </script>
 
-<style></style>
+<style lang="scss">
+.tools-list {
+  .tools-item-wrapper {
+    position: relative;
+    width: 33.33%;
+    height: 0;
+    padding-bottom: 33.33%;
+    display: inline-block;
+    .tools-item {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: calc(100% - 16px);
+      height: calc(100% - 16px);
+      background: #678fbb;
+      color: #fff;
+    }
+  }
+}
+</style>
